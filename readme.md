7979bfb1e256f43b7a5ca21e2ec78ff10eb63c8b
Ethan Rooke <ethan@roo.ke>
2022-07-12 10:56:29 -0500

    🎉 Initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+A minimal failing example for d3 with svelte. For some reason after the second
+press of the "Add Entry" button the scale of the chart no longer seems to
+update.
---
+++ src/App.svelte
@@ -0,0 +1,65 @@
+<script>
+  import {scaleLinear, extent} from 'd3';
+  import * as d3 from 'd3';
+
+  let entries = []
+
+  function addEntry() {
+    const i = entries.length + 1
+    entries = [ ...entries, {x: i, y: i * 10} ]
+    console.log(i)
+  }
+
+  // D3 Logic
+  const width = 800;
+  const height = 300;
+  $: {
+    const xScale = scaleLinear()
+      .domain(extent(entries, (e) => e.x))
+      .range([0, width])
+
+    const yScale = scaleLinear()
+      .domain(extent(entries, (e) => e.y))
+      .range([height,0])
+
+    d3.select("#chart")
+      .selectAll("circle")
+      .data(entries)
+      .enter()
+      .append("svg:circle")
+      .attr("cx", (e) => xScale(e.x))
+      .attr("cy", (e) => yScale(e.y))
+      .attr("r", 5)
+  }
+</script>
+
+<main>
+  <h1>D3 Example</h1>
+  <button on:click={addEntry}> Add Entry </button>
+  <div>
+  <svg id="chart" {width} {height}>
+  </svg>
+  </div>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
